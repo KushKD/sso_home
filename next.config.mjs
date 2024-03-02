e79c208a1384792b347a0c450eb8a0977f874de1
Kush Dhawan <kushkumardhawan@gmail.com>
2024-03-02 16:03:56 +0530

    final changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	assetPrefix: "/sso-integration",
-	basePath: "/sso-integration",
+  reactStrictMode: true,
+  assetPrefix: "/sso-integration",
+  basePath: "/sso-integration",
 };
 
 export default nextConfig;
